590ddd42bf7ba415a2140ea483191e6da4f70e7a
Franta Bartik <fb@franta.us>
2024-12-16 11:47:40 -0500

    Cross-Seed-Jeopardy tweaks

--- kubernetes/talos-maxi/apps/media/cross-seed-jeopardy/app/configs/config.js
+++ kubernetes/talos-maxi/apps/media/cross-seed-jeopardy/app/configs/config.js
@@ -4,29 +4,25 @@
 // Incomplete Save Path: /media/downloads/torrents/incomplete
 
 module.exports = {
-  delay: 30,
-  apiKey: process.env.CROSS_SEED_API_KEY,
-  transmissionRpcUrl: "http://transmission-jeopardy.media.svc.cluster.local:9091/transmission/rpc",
-
-  torznab: [
-    `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
-    `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
-    `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
-  ],
-
   action: "inject",
-  matchMode: "safe",
-  duplicateCategories: true,
-
-  includeNonVideos: true,
+  apiKey: process.env.CROSS_SEED_API_KEY,
+  delay: 30,
+  duplicateCategories: false,
+  includeEpisodes: true,
+  includeNonVideos: false,
   includeSingleEpisodes: true,
-
   linkCategory: "cross-seed",
-  linkType: "hardlink",
   linkDir: "/jeopardy/downloads/cross-seed",
-
-  outputDir: "/config",
-  torrentDir: "/transmission/torrents",
-
-  rssCadence: "15 minutes"
+  linkType: "hardlink",
+  matchMode: "safe",
+  outputDir: "/tmp",
+  port: Number(process.env.CROSS_SEED_PORT),
+  skipRecheck: true,
+  torrentDir: "/config/torrents",
+  transmissionRpcUrl: "http://transmission-jeopardy.media.svc.cluster.local:9091/transmission/rpc",
+  torznab: [],
+  //   `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
+  //   `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
+  //   `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
+  // ],
 };
--- kubernetes/talos-maxi/apps/media/cross-seed/app/configs/config.js
+++ kubernetes/talos-maxi/apps/media/cross-seed/app/configs/config.js
@@ -4,41 +4,33 @@
 // Incomplete Save Path: /media/downloads/torrents/incomplete
 
 module.exports = {
-  delay: 30,
-  apiKey: process.env.CROSS_SEED_API_KEY,
-  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
-
-  torznab: [
-    `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
-    `http://prowlarr.media.svc.cluster.local:80/60/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // HDB
-    `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
-    `http://prowlarr.media.svc.cluster.local:80/68/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // PTP
-    `http://prowlarr.media.svc.cluster.local:80/7/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // UHD
-    `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
-    `http://prowlarr.media.svc.cluster.local:80/69/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // AB
-  ],
-
   action: "inject",
-  matchMode: "safe",
-  duplicateCategories: true,
-
-  includeNonVideos: true,
-  includeEpisodes: true,
-  includeSingleEpisodes: true,
-
+  apiKey: process.env.CROSS_SEED_API_KEY,
   // The save paths for sonarr/radarr are set to the following:
   dataDirs: [
     "/media/downloads/tv-sonarr",
     "/media/downloads/radarr",
     "/media/downloads/anime-sonarr",
   ],
-
+  delay: 30,
+  duplicateCategories: true,
+  includeNonVideos: true,
+  includeEpisodes: true,
+  includeSingleEpisodes: true,
   linkCategory: "cross-seed",
   linkType: "hardlink",
   linkDir: "/media/downloads/xseed",
-
-  outputDir: "/config",
-  torrentDir: "/qbittorrent/qBittorrent/BT_backup",
-
-  rssCadence: "15 minutes"
+  matchMode: "safe",
+  outputDir: "/tmp",
+  torrentDir: "/config/qBittorrent/BT_backup",
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
+  torznab: [],
+  //   `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
+  //   `http://prowlarr.media.svc.cluster.local:80/60/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // HDB
+  //   `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
+  //   `http://prowlarr.media.svc.cluster.local:80/68/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // PTP
+  //   `http://prowlarr.media.svc.cluster.local:80/7/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // UHD
+  //   `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
+  //   `http://prowlarr.media.svc.cluster.local:80/69/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // AB
+  // ],
 };
