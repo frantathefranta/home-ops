af0cd8c2efade785e0f2f98a70c99debd8c76217
Franta Bartik <fb@franta.us>
2024-12-20 09:35:31 -0500

    Bringing torznab back

--- kubernetes/talos-maxi/apps/media/cross-seed/app/configs/config.js
+++ kubernetes/talos-maxi/apps/media/cross-seed/app/configs/config.js
@@ -24,13 +24,13 @@ module.exports = {
   outputDir: "/tmp",
   torrentDir: "/config/qBittorrent/BT_backup",
   qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
-  torznab: [],
-  //   `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
-  //   `http://prowlarr.media.svc.cluster.local:80/60/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // HDB
-  //   `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
-  //   `http://prowlarr.media.svc.cluster.local:80/68/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // PTP
-  //   `http://prowlarr.media.svc.cluster.local:80/7/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // UHD
-  //   `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
-  //   `http://prowlarr.media.svc.cluster.local:80/69/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // AB
-  // ],
+  torznab: [
+    `http://prowlarr.media.svc.cluster.local:80/2/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // BHD
+    `http://prowlarr.media.svc.cluster.local:80/60/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // HDB
+    `http://prowlarr.media.svc.cluster.local:80/67/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // MTV
+    `http://prowlarr.media.svc.cluster.local:80/68/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // PTP
+    `http://prowlarr.media.svc.cluster.local:80/7/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,   // UHD
+    `http://prowlarr.media.svc.cluster.local:80/66/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // BTN
+    `http://prowlarr.media.svc.cluster.local:80/69/api?apikey=$${process.env.PROWLARR__AUTH__APIKEY}`,  // AB
+  ],
 };
