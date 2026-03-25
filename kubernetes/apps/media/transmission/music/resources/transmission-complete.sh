b883da332006017fc45a4b4cff28e88ee51e4983
Franta Bartik <fb@franta.us>
2026-03-25 14:48:15 -0400

    Tweaks to transmission-complete script

--- kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
+++ kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
@@ -5,11 +5,14 @@ DEST_BASE="/music/transmission/wrtag"
 # WRTAG_API_KEY="key"   # replace with your actual wrtag API key
 # WRTAG_URL="http://:${WRTAG_API_KEY}@127.0.0.1:7373/op/move"
 
-SOURCE="${TR_TORRENT_DIR}/${TR_TORRENT_NAME}"
-DEST="${DEST_BASE}/${TR_TORRENT_NAME}"
+SOURCE="$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}"
+DEST="$${DEST_BASE}/$${TR_TORRENT_NAME}"
 
-echo "[transmission-complete] Copying '${SOURCE}' → '${DEST}'"
-cp -r "${SOURCE}" "${DEST}"
+echo "[transmission-complete] Generating gazelle-origin file for $${TR_TORRENT_NAME}"
+/app/.local/bin/gazelle-origin --tracker red "$${TR_TORRENT_HASH}" --out "$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}/origin.yaml"
+
+echo "[transmission-complete] Copying '$${SOURCE}' → '$${DEST}'"
+cp -rp "$${SOURCE}" "$${DEST}"
 
 # echo "[transmission-complete] Notifying wrtag: ${DEST}"
 # curl \
