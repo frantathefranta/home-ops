8c4de0c52ab8323122b8e2eea07cbc94eafa6868
Franta Bartik <fb@franta.us>
2026-03-27 10:26:20 -0400

    Adding logic to script to handle Orpheus

--- kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
+++ kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
@@ -8,8 +8,17 @@ DEST_BASE="/music/transmission/wrtag"
 SOURCE="$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}"
 DEST="$${DEST_BASE}/$${TR_TORRENT_NAME}"
 
-echo "[transmission-complete] Generating gazelle-origin file for $${TR_TORRENT_NAME}"
-/app/.local/bin/gazelle-origin --tracker red "$${TR_TORRENT_HASH}" --out "$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}/origin.yaml"
+if echo "$${TR_TORRENT_TRACKERS}" | grep -q "flacsfor.me"; then
+    TRACKER="red"
+elif echo "$${TR_TORRENT_TRACKERS}" | grep -q "home.opsfet.ch"; then
+    TRACKER="ops"
+else
+    echo "[transmission-complete] Unknown tracker: $${TR_TORRENT_TRACKERS}"
+    exit 1
+fi
+
+echo "[transmission-complete] Generating gazelle-origin file for $${TR_TORRENT_NAME} (tracker: $${TRACKER})"
+/app/.local/bin/gazelle-origin --tracker "$${TRACKER}" "$${TR_TORRENT_HASH}" --out "$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}/origin.yaml"
 
 echo "[transmission-complete] Copying '$${SOURCE}' → '$${DEST}'"
 cp -rp "$${SOURCE}" "$${DEST}"
