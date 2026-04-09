d2d101db7cd43bf43a95da41058f9b6d80168c3f
Franta <64412753+frantathefranta@users.noreply.github.com>
2026-04-09 13:31:21 -0400

    Adding nemorosa (#3137)
    * Adding nemorosa
    
    * Fixed port
    
    Fixed url in nemorosa config
    
    Fixed transmission-music endpoint URL
    
    Added nemorosa probes
    
    Fixed nemorosa port
    
    Trying an https endpoint for nemorosa
    
    Changing port for nemorosa
    
    Testing without securityContext on nemorosa
    
    Setting debug logging on nemorosa
    
    Moving tm and nemorosa to same service controller
    
    Enabling download for nemorosa
    
    Changing to HTTP port again
    
    Revert "Changing to HTTP port again"
    
    This reverts commit 79400d4eb78d27a091807469cdefc24371ae1d8d.
    
    Fixed transmission-complete script with nemorosa

--- kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
+++ kubernetes/apps/media/transmission/music/resources/transmission-complete.sh
@@ -13,7 +13,7 @@ fi
 echo "[transmission-complete] Generating gazelle-origin file for $${TR_TORRENT_NAME} (tracker: $${TRACKER})"
 /app/.local/bin/gazelle-origin --tracker "$${TRACKER}" "$${TR_TORRENT_HASH}" --out "$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}/origin.yaml"
 
-if [[ "$${TR_TORRENT_DIR}" != *"/lidarr"* ]]; then
+if [[ "$${TR_TORRENT_DIR}" == "/music/downloads" ]]; then
     WRTAG_COPY_URL="http://:$${WRTAG_API_KEY}@wrtag.media.svc.cluster.local/op/copy"
     TORRENT_PATH="$${TR_TORRENT_DIR}/$${TR_TORRENT_NAME}"
     echo "[transmission-complete] Notifying wrtag (copy) for: $${TORRENT_PATH}"
